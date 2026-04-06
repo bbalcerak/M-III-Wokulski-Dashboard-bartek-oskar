5f54aafd108a294c9bff2d09cd27e90ec90895eb
Oskarnuch <oskar.chenski@cyfroweszkoly.pl>
2026-04-06 11:50:32 +0200

    Add visual changes to main screen

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,13 +1,21 @@
 package com.example.wokolskidashboard.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.wokolskidashboard.model.Transaction
 import com.example.wokolskidashboard.ui.components.ExpenseForm
@@ -19,18 +27,41 @@ fun MainScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         val balance = transactions.sumOf { if (it.isExpense) -it.amount else it.amount }
 
-        Text(text = "Saldo: $balance rubli", modifier = Modifier.padding(16.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            Column(
+                Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("STAN KASY WOKULSKIEGO")
+                Text("${balance} Rubli")
+            }
+        }
 
         ExpenseForm(onAddExpense = { newTransaction ->
             transactions.add(newTransaction)
         })
 
-        Text(text = "Ostatnie zapisy:")
+        Text(text = "HISTORIA KSIĘGI: ",
+            modifier = Modifier.padding(8.dp),
+            fontWeight = FontWeight.Bold)
         LazyColumn {
-            items(transactions.size) { index ->
-                val item = transactions[index]
+            items(transactions) { item ->
 
-                Text("- ${item.name}: ${item.amount} rubli (${item.category})")
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(item.name)
+                    Text(
+                        text = "-${item.amount} rub",
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
     }
